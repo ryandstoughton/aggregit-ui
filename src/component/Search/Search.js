dcad7168e2d65e4afd15ca4239101dcdf410539a
Ryan Stoughton <ryandstoughton@gmail.com>
2020-02-04 22:20:07 -0600

    Convert project to use Sass; Add folder structure; Create Search component

---
+++ src/component/Search/Search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Search.module.scss';
+
+const search = () => {
+  return (
+    <div>Search is working!</div>
+  );
+}
+
+export default search;
---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.module.scss';
+import Search from '../component/Search/Search';
+
+const app = () => {
+  return (
+    <div>
+      <Search></Search>
+    </div>
+  );
+}
+
+export default app;

5169dc7f71f0aac47ce693c43fd3e35f8e729d32
Ryan Stoughton <ryandstoughton@gmail.com>
2020-03-30 19:57:06 -0500

    Add test files and eslint config

--- src/components/PlatformSelector/PlatformSelector.js
+++ src/components/PlatformSelector/PlatformSelector.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import Select from 'react-select';
+import React from "react";
+import Select from "react-select";
 
-const PlatformSelector = props => {
+const PlatformSelector = (props) => {
   const platforms = [
-    {value: 'github', label: 'Github'},
-    {value: 'gitlab', label: 'Gitlab'}
+    { value: "github", label: "Github" },
+    { value: "gitlab", label: "Gitlab" },
   ];
 
   const customStyles = {
     control: (base) => ({
       ...base,
-      width: '110px',
-      height: '40px',
-      'min-height': '40px',
-      'border-radius': '0',
-      'border-right': '0'
+      width: "110px",
+      height: "40px",
+      "min-height": "40px",
+      "border-radius": "0",
+      "border-right": "0",
     }),
-};
+  };
 
   return (
     <Select
       styles={customStyles}
-      defaultValue={platforms.find(platform => platform.value === 'github')}
+      defaultValue={platforms.find((platform) => platform.value === "github")}
       options={platforms}
-      onChange={props.changed}/>
+      onChange={props.changed}
+    />
   );
 };
 
